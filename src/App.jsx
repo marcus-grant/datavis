ab2a75a11a8002035b3d4643b3f0eccbd2bea825
Marcus Grant <marcus.grant@patternbuffer.io>
2017-12-04 03:15:20 -0500

    remove bootstrap app, include Workspace for charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
 import React /* , { Component } */ from 'react';
-import logo from './logo.svg';
+import Workspace from './workspace';
 import './App.css';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+// import logo from './logo.svg';
 
 const App = () => (
   <div className="App">
+    <Workspace />
+    {/*
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
@@ -27,6 +14,7 @@ const App = () => (
     <p className="App-intro">
       To get started, edit <code>src/App.js</code> and save to reload.
     </p>
+    */}
   </div>
 );
 export default App;
