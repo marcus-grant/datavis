12077f0876cabdb8b8e7a2de62a71b0e8d3ddc0e
Marcus Grant <marcus.grant@patternbuffer.io>
2017-12-11 01:53:25 -0500

    comment testProps, testData, Add basic PieChart

--- src/chart.jsx
+++ src/chart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ORFrame } from 'semiotic';
-import { testCSVString, CSVToObjectArray } from './csv-parser';
+// import { testCSVString, CSVToObjectArray } from './csv-parser';
 // import PropTypes from 'prop-types';
 
 // export const ChartType = {
@@ -15,14 +15,14 @@ import { testCSVString, CSVToObjectArray } from './csv-parser';
 // };
 //
 
-// const testData = [
-//   { name: 'Rick', score: 200 },
-//   { name: 'Morty', score: 20 },
-//   { name: 'Morty', score: 18 },
-//   { name: 'Summer', score: 22 },
-//   { name: 'Beth', score: 51 },
-//   { name: 'Jerry', score: 5 },
-// ];
+const testData = [
+  { name: 'Rick', score: 200 },
+  { name: 'Morty', score: 20 },
+  { name: 'Morty', score: 18 },
+  { name: 'Summer', score: 22 },
+  { name: 'Beth', score: 51 },
+  { name: 'Jerry', score: 5 },
+];
 
 // TODO: Needs better name, and potentially should be replaced by more integrated solution
 // In the case of some charts like Pie/Donut...
@@ -45,21 +45,32 @@ const reduceDataBySummingSameOrdinalEntries = (data, oKey, rKey) => {
   return reducedData;
 };
 
-const testPropsPie = {
-  size: [800, 480],
-  oAccessor: 'Grade',
-  dynamicColumnWidth: 'Volume',
-  type: 'bar',
-  projection: 'radial',
-  style: { fill: '#00a2ce', stroke: 'white' },
-  oLabel: true,
-  // data: testData,
-  // data: reduceDataBySummingSameOrdinalEntries(testData, 'name', 'score'),
-  data: reduceDataBySummingSameOrdinalEntries(CSVToObjectArray(testCSVString), 'Grade', 'Volume'),
-};
+// const testPropsPie = {
+//   size: [320, 320],
+//   oAccessor: 'Grade',
+//   dynamicColumnWidth: 'Volume',
+//   type: 'bar',
+//   projection: 'radial',
+//   style: { fill: '#00a2ce', stroke: 'white' },
+//   oLabel: true,
+//   data: testData,
+// data: reduceDataBySummingSameOrdinalEntries(testData, 'name', 'score'),
+// data: reduceDataBySummingSameOrdinalEntries(CSVToObjectArray(testCSVString), 'Grade', 'Volume'),
+// };
 
 // TODO: Create a data classifier that can test if a column of data can be represented as a number
 // This will make it easier to setup controls that have the user select data and chart types
 // TODO: Props should be better organized and categorized
 // TODO: Change to props input from workspace after testing
-export default () => <ORFrame {...testPropsPie} />;
+export const PieChart = props => (
+  <ORFrame
+    size={[360, 360]}
+    oAccessor="Part"
+    dynamicColumnWidth="Volume"
+    type="bar"
+    projection="radial"
+    style={{ fill: '#00a2ce', stroke: 'white' }}
+    data={props.chartData}
+    oLabel
+  />
+);
